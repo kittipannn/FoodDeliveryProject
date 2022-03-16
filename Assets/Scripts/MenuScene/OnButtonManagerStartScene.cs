dcd136b1109afbb3670062e0579ad65f96a57d44
kittipannn <kittipannn@hotmail.com>
2022-03-16 22:32:34 +0700

    Fixed Bug Text
    แก้ปัญหาพื้นหลังสีดำที่ตัวหนังสือ

--- Assets/Scripts/MenuScene/OnButtonManagerStartScene.cs
+++ Assets/Scripts/MenuScene/OnButtonManagerStartScene.cs
@@ -6,17 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class OnButtonManagerStartScene : MonoBehaviour
 {
+    [Header ("Button")]
     [SerializeField] Button startBtn;
     [SerializeField] Button tutorialBtn;
     [SerializeField] Button testBtn;
+    [SerializeField] Button optionBtn;
+    [SerializeField] Button closeOptionBtn;
+
+    [Header("GameObject")]
     [SerializeField] SceneLoader sceneLoader;
-    
+    [SerializeField] GameObject optionPanel;
 
     private void Start()
     {
+        //Setting Button
         startBtn.onClick.AddListener(() => onStartBtn());
         tutorialBtn.onClick.AddListener(() => onTutorialBtn());
         testBtn.onClick.AddListener(() => onTestBtn());
+        optionBtn.onClick.AddListener(() => onOptionBtn());
+        closeOptionBtn.onClick.AddListener(() => onOptionBtn());
     }
     void onStartBtn()
     {
@@ -30,5 +38,16 @@ public class OnButtonManagerStartScene : MonoBehaviour
     void onTestBtn() 
     {
         sceneLoader.LoadingScene("TestScene");
+    }
+    void onOptionBtn() 
+    {
+        if (!optionPanel.activeInHierarchy)
+        {
+            optionPanel.SetActive(true);
+        }
+        else
+        {
+            optionPanel.SetActive(false);
+        }
     }
 }
