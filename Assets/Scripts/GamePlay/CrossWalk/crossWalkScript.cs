d2823a5795887d8c0aa7225f56923b0c5fdf7ef3
kittipannn <kittipannn@hotmail.com>
2022-04-02 17:26:24 +0700

    Added animator to script Crosswalk And Change UI

--- Assets/Scripts/GamePlay/CrossWalk/crossWalkScript.cs
+++ Assets/Scripts/GamePlay/CrossWalk/crossWalkScript.cs
@@ -10,6 +10,7 @@ public class crossWalkScript : MonoBehaviour
     bool countTime = true;
     bool crosswalk = false;
     float currentTime;
+    Animator animator;
     private void Start()
     {
         this.enabled = false;
@@ -17,6 +18,7 @@ public class crossWalkScript : MonoBehaviour
     private void OnEnable()
     {
         rb = this.GetComponent<Rigidbody>();
+        animator = gameObject.GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -37,6 +39,7 @@ public class crossWalkScript : MonoBehaviour
         if (crosswalk)
         {
             //rb.velocity = Vector3.right * speed;
+            animator.SetBool("IsMovingTrigger", true);
             rb.AddRelativeForce(Vector3.right * speed);
         }
     }
@@ -44,6 +47,7 @@ public class crossWalkScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Stopp"))
         {
+            animator.SetBool("IsMovingTrigger", false);
             rb.velocity = Vector3.zero;
             this.enabled = false;
         }
